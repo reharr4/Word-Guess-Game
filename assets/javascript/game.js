db40987a4ca692c3635a018a85c6adadb909b963
Harrison, Rachel <reharr4@gmail.com>
2019-05-08 12:03:31 -0500

    variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var him = {
+
+}
+var mojoJojo = {
+
+}
+var sedusa = {
+
+}
+var amoebaBoys = {
+
+}
+var fuzzyLumpkins = {
+
+}
+var princessMorbucks = {
+
+}
+var ganggreenGang = {
+
+}
+var rowdyruffBoys = {
+
+}
+
+if ()
+function alreadyGuessed() {
+  console.log();
+  console.log();
+  console.log ();
+  console.log();
+}
+
+document.onkeyup = function(event) {
+  var him = event.key.toLowerCase();
+  if (letter === ) {
+    
+  }
+}
