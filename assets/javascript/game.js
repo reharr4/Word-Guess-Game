455dbc0d8d68692a9887fd0c8e0721ef62c65105
Harrison, Rachel <reharr4@gmail.com>
2019-05-08 20:22:30 -0500

    some more variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,29 @@
 // Global Variables
+// =========================================================================
 
 var villainArray = ["Him", "Mojo Jojo", "Fuzzy Lumpkins", "Princess Morbucks", "The Ganggreen Gang", "The Amoeba Boys", "Sedusa", "The Rowdyruff Boys"];
 
 // https://www.kirupa.com/html5/picking_random_item_from_array.htm
-var randomValue = villainArray[Math.floor(Math.random() * villainArray.length)];
-console.log("random word", randomValue);
+var randomWord = villainArray[Math.floor(Math.random() * villainArray.length)];
+console.log("random word", randomWord);
 
-var wordLength = randomValue.length;
+// wordLength = x
+var wordLength = randomWord.length;
 
 // guesses 20
-var guessed = guessed(wordLength)
+var guessed = guessed(wordLength);
 
 // guessedLetters = ["x", "y", "z"]
 var guessedLetters = [];
+
+// randomWordLetters = ["M", "o", "j", "o", "J", "o", "j", "o"]
+var randomWordLetters = [];
 
 // userGuess = "x"
 var userGuess;
 
 // Functions
-
+// ==============================================================
 function guess(x, y) {
     return x + y;
 }
