f119180af087e642d29999d5ee7244e6be66dd3d
Harrison, Rachel <reharr4@gmail.com>
2019-05-10 18:26:58 -0500

    make work functions plz

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,19 @@
 // Global Variables
 // =========================================================================
 // Available choices
-var villainArray = ["Him", "Mojo Jojo", "Fuzzy Lumpkins", "Princess Morbucks", "The Ganggreen Gang", "The Amoeba Boys", "Sedusa", "The Rowdyruff Boys"];
-// letter choices
+var villainArray = ["Him", 
+  "Mojo Jojo", 
+  "Fuzzy Lumpkins", 
+  "Princess Morbucks", 
+  "The Ganggreen Gang", 
+  "The Amoeba Boys", 
+  "Sedusa", 
+  "The Rowdyruff Boys"
+];
+
+/*// letter choices
 var letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+*/
 
 // score
 var wins = 0;
@@ -11,22 +21,17 @@ var losses = 0;
 var guesses = 9;
 var guessesRemaining = 20;
 var guessedLetters = [];
-var letterToGuess = null;
-
-// guesses remaining
-var updateguessesRemaining = function() {
-  document.querySelector('#guessesRemaining').innerHTML = "Guesses Remaining: " + guessesRemaining;
-  console.log("guesses remaining", guessesRemaining);
-};
+var wordToGuess = null;
 
 // words to guess
-var updateletterToGuess = function(){
-  this.letterToGuess = this.letterChoices[Math.floor(Math.random() * this.letterChoices.length)];
-  console.log("letters to guess", letterToGuess);
+var updatewordToGuess = function(){
+  this.wordToGuess = this.villainArray[Math.floor(Math.random() * this.villainArray.length)];
+  console.log("letters to guess", wordToGuess);
 };
 
-var updateGuessesSoFar = function(){
+var updateplayerGuess = function(){
   document.querySelector('#let').innerHTML = "Guesses so far: " + guessedLetters.join(', ');
+  console.log("letters already guessed", guessedLetters);
 };
 
 // computer chooses word
@@ -34,20 +39,20 @@ var updateGuessesSoFar = function(){
 var computerChoice = villainArray[Math.floor(Math.random() * villainArray.length)];
 console.log("random word", computerChoice);
 
-/*// wordLength = x
-var wordLength = randomWord.length;
+// answers
+var answerArray = [];
+for (var i = 0; i < villainArray.length; i++) {
+  answerArray[i] = "_";
+  console.log("answer");
+}
 
-// guesses 20
-var totalGuesses = guessed(wordLength);
+var remainingLetters = villainArray.length;
 
-// guessedLetters = ["x", "y", "z"]
-var guessedLetters = [];
-
-// randomWordLetters = ["M", "o", "j", "o", "J", "o", "j", "o"]
-var randomWordLetters = [];
-
-// playerGuess = "x"
-var playerGuess;*/
+// guesses remaining
+var updateguessesRemaining = function() {
+  document.querySelector('#guessesRemaining').innerHTML = "Guesses Remaining: " + guessesRemaining;
+  console.log("guesses remaining", guessesRemaining);
+};
 
 // reset
 var reset = function(){
@@ -66,38 +71,22 @@ updateplayerGuess();
 
 // Functions
 // ==============================================================
-/*function guessed(x, y) {
-    return x + y;
-}
-
-/*function wrongGuess() {
-  guessedLetters.push(playerGuess);
-  document.getElementById("letters guessed") = guessedLetters();
-  guessed--;
-  console.log("wrong guess", guessed);
-}
-
-function correctGuess(indexes) {
-  document.getElementById(villainArray);
-  guessed--;
-  console.log("correct guess", guessed);
-}*/
-
 
 // player input
 
 // https://www.w3schools.com/jsref/prop_html_innerhtml.asp
-document.onkeyup = function(event) {
+  document.onkeyup = function(event) {
   guessesRemaining--;
-  var playerGuess = event.key;
+  var playerGuess = event.key.toLowerCase();
   console.log(playerGuess);
 
   guessedLetters.push(playerGuess);
-  updateGuessesRemaining();
+  updateguessesRemaining();
   updateguessedLetters();
 
     if (guessesRemaining > 0) {
-      if (playerGuess === letterToGuess){
+      answerArray.join(" ");
+      if (playerGuess === wordToGuess){
           wins++;
           document.querySelector('#wins').innerHTML = 'Wins: ' + wins;
           alert("Powerpuffs Save the Day!");
