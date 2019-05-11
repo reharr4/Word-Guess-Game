6f2ad8ce253cf77bea3312a8d4f3a6a18185cfcf
Harrison, Rachel <reharr4@gmail.com>
2019-05-10 22:53:22 -0500

    logging letters guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // Global Variables
 // =========================================================================
 // Available choices
+/*var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];*/
 var villainArray = ["Him", 
   "Mojo Jojo", 
   "Fuzzy Lumpkins", 
@@ -11,10 +12,6 @@ var villainArray = ["Him",
   "The Rowdyruff Boys"
 ];
 
-/*// letter choices
-var letterChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-*/
-
 // score
 var wins = 0;
 var losses = 0;
@@ -24,34 +21,23 @@ var wordToGuess = villainArray;
 
 // words to guess
 var updatewordToGuess = function(){
-  this.wordToGuess = this.villainArray[Math.floor(Math.random() * this.villainArray.length)];
-  console.log("letters to guess", wordToGuess);
+  this.wordToGuess = this.letterChoices[Math.floor(Math.random() * this.letterChoices.length)];
 };
 
 var updateguessesSoFar = function(){
-  document.querySelector('#let').innerHTML = "Guesses so far: " + guessesSoFar.join(', ');
-  console.log("guesses so far", guessesSoFar);
+  document.querySelector('#guessesSoFar').innerHTML="Guesses so far: " + guessesSoFar.join(' , ');
 };
 
 // computer chooses word
 // https://www.kirupa.com/html5/picking_random_item_from_array.htm
 var computerChoice = villainArray[Math.floor(Math.random() * villainArray.length)];
-console.log("random word", computerChoice);
-
-// answers
-var answerArray = [];
-for (var i = 0; i < villainArray.length; i++) {
-  answerArray[i] = "_";
-  console.log("answer");
-}
-
-/*var remainingLetters = villainArray.length;*/
 
 // guesses remaining
 var updateguessesRemaining = function() {
-  document.querySelector('#guessesRemaining').innerHTML = "Guesses Remaining: " + guessesRemaining;
-  console.log("guesses remaining", guessesRemaining);
+  document.querySelector('#guessesRemaining').innerHTML="Guesses Remaining: " + guessesRemaining;
 };
+
+var remainingLetters = villainArray.length;
 
 // reset
 var reset = function(){
@@ -64,36 +50,38 @@ var reset = function(){
 
 };
 
-updateguessesRemaining();
-updateguessesSoFar();
-
+function reWriteStats(){
+console.log("guessesRemaining", guessesRemaining);
+console.log("computerChoice", computerChoice);
+console.log("losses", losses);
+console.log("wins", wins);
+console.log("guessesSoFar", guessesSoFar);
+console.log("wordToGuess", wordToGuess);
+}
 
 // player input
 
 // https://www.w3schools.com/jsref/prop_html_innerhtml.asp
 document.onkeyup = function(event) {
-  guessesRemaining--;
+  
   var playerGuess = event.key.toLowerCase();
-  console.log(playerGuess);
-
+    
   guessesSoFar.push(playerGuess);
   updateguessesRemaining();
   updateguessesSoFar();
 
-    if (guessesRemaining > 0) {
-        // answerArray.join(" ");
-      if (playerGuess === wordToGuess){
+  if (playerGuess == computerChoice) {
           wins++;
           document.querySelector('#wins').innerHTML = 'Wins: ' + wins;
           alert("Powerpuffs Save the Day!");
-          console.log(wins);
+          reWriteStats();
+
           reset();
-        } 
-    } else if (guessesRemaining === 0) {
+    } else if (guessesRemaining == 0) {
           losses++;
           document.querySelector('#losses').innerHTML = 'Losses: ' + losses;
           alert("The City of Townsville is under attack!");
-          console.log(losses);
+          reWriteStats();
 
           reset();
     }
